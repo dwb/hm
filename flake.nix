d43db06d427a7e10eedad2976e9808b28bb780f4
Dani Brown <d@dani.cool>
2024-07-01 13:10:04 +0100

    use release-24.05 branch for nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.05";
+      url = "github:NixOS/nixpkgs/release-24.05";
     };
     nixpkgsUnstable = {
       url = "github:NixOS/nixpkgs";
