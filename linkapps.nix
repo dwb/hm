c707df52c4640e65d5b35202a53eda1a2df2937f
Dani Brown <dani@causal.app>
2025-06-24 09:39:23 +0100

    linkapps

--- linkapps.nix
+++ linkapps.nix
@@ -6,14 +6,14 @@
     file."Applications/Home Manager Apps".enable = false;
 
     activation.aliasHomeManagerApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      app_folder="${config.home.homeDirectory}/Applications"
-      mkdir -p "$app_folder"
+      targetDir="${config.home.homeDirectory}/Applications"
+      run mkdir -p "$targetDir"
       shopt -s nullglob
-      for app in "$genProfilePath"/home-path/*.app "$genProfilePath"/home-path/Applications/*.app; do
+      for app in "$newGenPath"/home-path{,/Applications}/*.app; do
         app="$(realpath "$app")"
-        app_target="$app_folder/$(basename "$app")"
-        if [[ -f $app_target ]]; then run rm "$app_target"; fi
-        run ${pkgs.mkalias}/bin/mkalias -v "$app" "$app_target"
+        targetApp="$targetDir/$(basename "$app")"
+        if [[ -f $targetApp ]]; then run rm "$targetApp"; fi
+        run ${pkgs.mkalias}/bin/mkalias -v "$app" "$targetApp"
       done
     '';
   };
