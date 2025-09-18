292779085efb3336a3a220555e6536ff26861dae
Dani Brown <dani@causal.app>
2025-09-18 09:15:06 +0100

    fix jj rp warning

--- jj-commands/rm-parent.py
+++ jj-commands/rm-parent.py
@@ -16,5 +16,5 @@ args = parser.parse_args()
 
 subprocess.run([
     'jj', 'rebase', '-s', args.revision,
-    '-d', f'all:{args.revision}- ~ {args.parent}',
+    '-d', f'{args.revision}- ~ {args.parent}',
 ], check=True)
